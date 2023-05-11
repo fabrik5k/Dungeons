bda287b0930437a27acda2e4be789b2dfe668899
Fábio Oliveira <cbarrossilva123@gmail.com>
2023-05-11 20:32:35 -0300

    Primeira Sprint

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,16 @@ var linha = 135
 var GameOver = false
 
 // Controladores
-var controle = 'frente'
+var controle = "frente"
 var movendo = false
-var pegar = false
-
-// Jogador
-var player
+var acao = false
 
 
-function criarMapa()
+// Array das chaves do jogo
+var chave = []
+
+
+function iniciarMapa()
 {
     var mapa = []
 
@@ -34,9 +35,9 @@ function criarMapa()
             var points = document.getElementById(i)
             points.appendChild(document.createElement("td")).setAttribute("id", `${i}-${j}`)
             document.getElementById(`${i}-${j}`).setAttribute("class", "pixelMapa")
-            document.getElementById(`${i}-${j}`).setAttribute("height", "10px")
-            document.getElementById(`${i}-${j}`).setAttribute("width", "10px")
-            // document.getElementById(`${i}-${j}`).setAttribute();
+            document.getElementById(`${i}-${j}`)//.setAttribute("height", "10px")
+            document.getElementById(`${i}-${j}`)//.setAttribute("width", "10px")
+            //document.getElementById(`${i}-${j}`).textContent = `${i} ${j}`
             //document.getElementById(`${i}-${j}`).addEventListener() // Adicionar evento futuramente
         }
     }
@@ -45,43 +46,98 @@ function criarMapa()
 }
 iniciarJogo()
 
+
+function construirMapa1(mapa)
+{
+    // Colocar um switch nessa função
+    for (let i = 0; i < 135; i++) {
+        // Paredes
+        if (i < 109 && i > 42) {
+            if (i > 90) {
+                mapa[41][i] = 1
+                document.getElementById(`41-${i}`).textContent = '*'
+            }
+            mapa[27][i] = 1
+            document.getElementById(`27-${i}`).textContent = '*'
+            mapa[53][i] = 1
+            document.getElementById(`53-${i}`).textContent = '*'
+        }
+        if (i > 27 && i < 54) {
+            mapa[i][109] = 1
+            document.getElementById(`${i}-109`).textContent = '*'
+            mapa[i][42] = 1
+            document.getElementById(`${i}-42`).textContent = '*'
+            if (i > 40) {
+                mapa[i][90] = 1
+                document.getElementById(`${i}-90`).textContent = '*'
+            }
+        }        
+        
+    }
+    return mapa
+}
+
 function renderizacao(mapa, personagem)
 {
+    // Mudar pra um switch case usando a soma das coordenadas do personagem como caso
 
-    var boneco = document.querySelector(`#${personagem.x}-${personagem.y}`)
+    //if (acao && ()) {
+
+    //    acao = false
+    //}
+
+    // Condições e posicionamento da chave
+    if (acao && (personagem.x + personagem.y == chave[0].coordenada.x + chave[0].coordenada.y)) {
+        chave[0].playerComAChave = true
+        chave[0]._porta.textContent = '='
+        mapa[chave[0].porta.x][chave[0].porta.y] = 0
+        acao = false
+    } 
+    if (!chave[0].playerComAChave){
+        document.getElementById(`${chave[0].coordenada.x}-${chave[0].coordenada.y}`).textContent = '@'
+    } else {
+        chave[0]._porta.textContent = '='
+    }
+
+
+    // Movimentação do boneco
+    var boneco = document.getElementById(`${personagem.x}-${personagem.y}`)
     boneco.textContent = '&'
     if (movendo) {
         movendo = false
         switch (controle) {
             case 'frente':
-                if (mapa[personagem.x-1][personagem.y] == 0 && mapa[personagem.x-1][personagem.y]) {
+                if (mapa[personagem.x-1][personagem.y] == 0) {
                     boneco.textContent = ''
-                    boneco = document.querySelector(`#${personagem.x-1}-${personagem.y}`)
-                    console.log("asd")
+                    boneco = document.getElementById(`${personagem.x-1}-${personagem.y}`)
+                    boneco.textContent = '&'
                     return {x: personagem.x-1, y: personagem.y}
                 }
                 break;
 
             case 'tras':
-                if (mapa[personagem.x+1][personagem.y] == 0 && mapa[personagem.x+1][personagem.y]) {
+                if (mapa[personagem.x+1][personagem.y] == 0  ) { //&& mapa[personagem.x+1][personagem.y]
                     boneco.textContent = ''
-                    boneco = document.querySelector(`#${personagem.x+1}-${personagem.y}`)
+                    boneco = document.getElementById(`${personagem.x+1}-${personagem.y}`)
+                    boneco.textContent = '&'
                     return {x: personagem.x+1, y: personagem.y}
                 }
                 break;
 
             case 'esquerda':
-                if (mapa[personagem.x][personagem.y-1] == 0 && mapa[personagem.x][personagem.y-1]) {
+                if (mapa[personagem.x][personagem.y-1] == 0) { //&& mapa[personagem.x][personagem.y-1]
                     boneco.textContent = ''
-                    boneco = document.querySelector(`#${personagem.x}-${personagem.y-1}`)
+                    boneco = document.getElementById(`${personagem.x}-${personagem.y-1}`)
+                    boneco.textContent = '&'
                     return {x: personagem.x, y: personagem.y-1}
                 }
                 break;
             
             case 'direita':
-                if (mapa[personagem.x][personagem.y+1] == 0 && mapa[personagem.x][personagem.y+1]) {
+                if (mapa[personagem.x][personagem.y+1] == 0) { //&& mapa[personagem.x][personagem.y+1]
                     boneco.textContent = ''
-                    boneco = document.querySelector(`#${personagem.x}-${personagem.y+1}`)
+                    boneco = document.getElementById(`${personagem.x}-${personagem.y+1}`)
+                    boneco.textContent = '&'
                     return {x: personagem.x, y: personagem.y+1}
                 }
                 break;
@@ -90,10 +146,10 @@ function renderizacao(mapa, personagem)
     return {x: personagem.x, y: personagem.y}
 }
 
-
+// Inicializa as principais config do jogo
 function iniciarJogo()
 {
-    var mapa = criarMapa()
+    var mapa = iniciarMapa()
     var coordenadaInicial = {x: 50, y:50} // consertar depois
     var fase = 1
 
@@ -102,21 +158,29 @@ function iniciarJogo()
     
 }
 
-
+// Starta o jogo
 function start(mapa, fase, coordenadaInicial)
 {
+    var player = new Personagem(coordenadaInicial)
+    
     switch (fase) {
         case 1:
-            player = new Personagem(coordenadaInicial)
+            mapa = construirMapa1(mapa)
+            chave[0] = new Chave({x: 30, y:90}, false, {x: 47, y: 90})
 
-            while (!GameOver) {
+            const intervalId = setInterval(function() {
                 player.coordenada = renderizacao(mapa, player.coordenada)
-            }
+                if (GameOver) {
+                    clearInterval(intervalId)
+                }
+            }, 1);
+
             fase++
+            start(mapa, fase, coordenadaInicial)
             break;
     
         case 2:
-            
+           
             break;
 
         case 3:
@@ -154,7 +218,7 @@ document.addEventListener("keydown", function(event)
         break;
 
         case "KeyE":
-            
+            acao = true
         break;
     }
     event.preventDefault();
@@ -167,8 +231,14 @@ function Personagem(coordenadaInicial)
     this.coordenada = coordenadaInicial
 }
 
-function Chave(coordenada, player)
+function Chave(coordenada, playerComAChave, porta)
 {
     this.coordenada = coordenada
+    this.playerComAChave = playerComAChave
+    this.porta = porta
+    this._porta = document.getElementById(`${porta.x}-${porta.y}`)
+    this._porta.textContent = 'D'
+    
+   
 }
 
