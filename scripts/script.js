a2fd700144fb4be242bf2f4659f53ba299e6c023
Fábio Oliveira <cbarrossilva123@gmail.com>
2023-05-18 23:24:07 -0300

    Segunda Sprint

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,9 @@ var linha = 135
 
 // Estado do jogo
 var GameOver = false
+var morte = false
+var contadorMorte = 0
+var faseOver = false
 
 // Controladores
 var controle = "frente"
@@ -18,7 +21,8 @@ var acao = false
 
 
 // Array das chaves do jogo
-var chave = []
+var chave = [10]
+var botao
 
 
 function iniciarMapa()
@@ -47,34 +51,99 @@ function iniciarMapa()
 iniciarJogo()
 
 
-function construirMapa1(mapa)
+function construirMapa1(mapa, fase)
 {
-    // Colocar um switch nessa função
-    for (let i = 0; i < 135; i++) {
-        // Paredes
-        if (i < 109 && i > 42) {
-            if (i > 90) {
-                mapa[41][i] = 1
-                document.getElementById(`41-${i}`).textContent = '*'
-            }
-            mapa[27][i] = 1
-            document.getElementById(`27-${i}`).textContent = '*'
-            mapa[53][i] = 1
-            document.getElementById(`53-${i}`).textContent = '*'
+    for (let i = 0; i < 68; i++) {
+        for (let j = 0; j < 135; j++) {
+            mapa[i][j] = 0
+            document.getElementById(`${i}-${j}`).textContent = ''
         }
-        if (i > 27 && i < 54) {
-            mapa[i][109] = 1
-            document.getElementById(`${i}-109`).textContent = '*'
-            mapa[i][42] = 1
-            document.getElementById(`${i}-42`).textContent = '*'
-            if (i > 40) {
-                mapa[i][90] = 1
-                document.getElementById(`${i}-90`).textContent = '*'
-            }
-        }        
         
     }
-    return mapa
+
+    switch (fase) {
+        case 1:
+            for (let i = 0; i < 135; i++) {
+                // Paredes
+                if (i >= 15 && i <= 30) {
+                    //if (i > 90) {
+                        //mapa[41][i] = 1
+                        //document.getElementById(`41-${i}`).textContent = '*'
+                   // }
+                    mapa[i][40] = 1
+                    document.getElementById(`${i}-40`).textContent = '*'
+                    mapa[i][55] = 1
+                    document.getElementById(`${i}-55`).textContent = '*'
+                }
+                if (i >= 40 && i <= 55) {
+                    mapa[30][i] = 1
+                    document.getElementById(`30-${i}`).textContent = '*'
+                    mapa[15][i] = 1
+                    document.getElementById(`15-${i}`).textContent = '*'
+                    //if (i > 40) {
+                        //mapa[i][90] = 1
+                        //document.getElementById(`${i}-90`).textContent = '*'
+                    //}
+                }        
+                
+            }
+            return mapa
+            
+    
+        case 2:
+            mapa[23][61] = 2
+            mapa[29][57] = 2
+            mapa[29][71] = 2
+            mapa[21][76] = 2
+            mapa[21][78] = 2
+            mapa[18][56] = 2
+            mapa[21][54] = 2
+            mapa[43][65] = 2
+            mapa[39][59] = 2
+            mapa[45][67] = 2
+            for (let i = 0; i < 68; i++) {
+                for (let j = 0; j < 135; j++) {
+                    if (mapa[i][j] == 2) {
+                        document.getElementById(`${i}-${j}`).textContent = '#'
+                    }
+                }
+                
+            }
+
+            for (let i = 0; i < 135; i++) {
+                if (i >= 15 && i <= 45) {
+                    if(i < 24){
+                        mapa[i][72] = 1
+                        document.getElementById(`${i}-${72}`).textContent = '*'
+                        mapa[i][57] = 1
+                        document.getElementById(`${i}-${57}`).textContent = '*'
+                    }
+                    mapa[i][50] = 1
+                    document.getElementById(`${i}-${50}`).textContent = '*'
+                    mapa[i][80] = 1
+                    document.getElementById(`${i}-${80}`).textContent = '*'
+                }
+                if (i >= 50 && i<= 80){
+                    if (i <= 57) {
+                        mapa[23][i] = 1
+                        document.getElementById(`${23}-${i}`).textContent = '*' 
+                    }
+                    if (i>=73) {
+                        mapa[23][i] = 1
+                        document.getElementById(`${23}-${i}`).textContent = '*'
+                    }
+                    mapa[45][i] = 1
+                    document.getElementById(`${45}-${i}`).textContent = '*'
+                    mapa[15][i] = 1
+                    document.getElementById(`${15}-${i}`).textContent = '*'
+                }
+                
+            }
+            return mapa
+            
+    }
+    // Colocar um switch nessa função
+
 }
 
 function renderizacao(mapa, personagem)
@@ -87,18 +156,35 @@ function renderizacao(mapa, personagem)
     //}
 
     // Condições e posicionamento da chave
-    if (acao && (personagem.x + personagem.y == chave[0].coordenada.x + chave[0].coordenada.y)) {
-        chave[0].playerComAChave = true
-        chave[0]._porta.textContent = '='
-        mapa[chave[0].porta.x][chave[0].porta.y] = 0
-        acao = false
-    } 
-    if (!chave[0].playerComAChave){
-        document.getElementById(`${chave[0].coordenada.x}-${chave[0].coordenada.y}`).textContent = '@'
-    } else {
-        chave[0]._porta.textContent = '='
+    // Ajeitar futuramente
+    for (let i = 0; i < chave.length; i++) {
+        if (acao && (personagem.x == chave[i].coordenada.x && personagem.y == chave[i].coordenada.y)) {
+            chave[i].playerComAChave = true
+            chave[i]._porta.textContent = '='
+            mapa[chave[i].porta.x][chave[i].porta.y] = 0
+        } 
+        if (!chave[i].playerComAChave){
+            document.getElementById(`${chave[i].coordenada.x}-${chave[i].coordenada.y}`).textContent = '@'
+        } else {
+            chave[i]._porta.textContent = '='
+        }
+        
     }
 
+    if (botao != null && acao && ((botao.coordenada.x == personagem.x) && (botao.coordenada.y == personagem.y))) {
+        if (mapa[botao.porta.x][botao.porta.y] == 1) {
+            mapa[botao.porta.x][botao.porta.y] = 0
+            botao._porta.textContent = ''
+        } else {
+            mapa[botao.porta.x][botao.porta.y] = 1
+            botao._porta.textContent = '*'
+        }
+        
+    } else if(botao != null) {
+        document.getElementById(`${botao.coordenada.x}-${botao.coordenada.y}`).textContent = 'O'
+    }
+
+    acao = false
 
     // Movimentação do boneco
     var boneco = document.getElementById(`${personagem.x}-${personagem.y}`)
@@ -107,7 +193,9 @@ function renderizacao(mapa, personagem)
         movendo = false
         switch (controle) {
             case 'frente':
-                if (mapa[personagem.x-1][personagem.y] == 0) {
+                if (mapa[personagem.x-1][personagem.y] == 2) {
+                    morte = true
+                } else if (mapa[personagem.x-1][personagem.y] == 0) {
                     boneco.textContent = ''
                     boneco = document.getElementById(`${personagem.x-1}-${personagem.y}`)
                     boneco.textContent = '&'
@@ -116,7 +204,9 @@ function renderizacao(mapa, personagem)
                 break;
 
             case 'tras':
-                if (mapa[personagem.x+1][personagem.y] == 0  ) { //&& mapa[personagem.x+1][personagem.y]
+                if (mapa[personagem.x+1][personagem.y] == 2) {
+                    morte = true
+                } else if (mapa[personagem.x+1][personagem.y] == 0  ) { //&& mapa[personagem.x+1][personagem.y]
                     boneco.textContent = ''
                     boneco = document.getElementById(`${personagem.x+1}-${personagem.y}`)
                     boneco.textContent = '&'
@@ -125,7 +215,9 @@ function renderizacao(mapa, personagem)
                 break;
 
             case 'esquerda':
-                if (mapa[personagem.x][personagem.y-1] == 0) { //&& mapa[personagem.x][personagem.y-1]
+                if (mapa[personagem.x][personagem.y-1] == 2) {
+                    morte = true
+                } else if (mapa[personagem.x][personagem.y-1] == 0) { //&& mapa[personagem.x][personagem.y-1]
                     boneco.textContent = ''
                     boneco = document.getElementById(`${personagem.x}-${personagem.y-1}`)
                     boneco.textContent = '&'
@@ -134,7 +226,9 @@ function renderizacao(mapa, personagem)
                 break;
             
             case 'direita':
-                if (mapa[personagem.x][personagem.y+1] == 0) { //&& mapa[personagem.x][personagem.y+1]
+                if (mapa[personagem.x][personagem.y+1] == 2) {
+                    morte = true
+                } else if (mapa[personagem.x][personagem.y+1] == 0) { //&& mapa[personagem.x][personagem.y+1]
                     boneco.textContent = ''
                     boneco = document.getElementById(`${personagem.x}-${personagem.y+1}`)
                     boneco.textContent = '&'
@@ -165,22 +259,52 @@ function start(mapa, fase, coordenadaInicial)
     
     switch (fase) {
         case 1:
-            mapa = construirMapa1(mapa)
-            chave[0] = new Chave({x: 30, y:90}, false, {x: 47, y: 90})
+            player.coordenada = {x:27, y:48}
+            mapa = construirMapa1(mapa, fase)
+            chave[0] = new Chave({x: 17, y:52}, false, {x: 30, y: 48})
 
             const intervalId = setInterval(function() {
                 player.coordenada = renderizacao(mapa, player.coordenada)
-                if (GameOver) {
+                if (morte == true) {
+                    contadorMorte++
+                    morte = false
+                    start(mapa, fase, coordenadaInicial)
+                } else if(contadorMorte == 3){
                     clearInterval(intervalId)
+                    window.location.replace("../templated/home.html") // Pagina do game over de derrota colocar depois
+                } else if ( (player.coordenada.x == chave[0].porta.x) && (player.coordenada.y == chave[0].porta.y) ) {
+                    clearInterval(intervalId)
+                    fase++
+                    start(mapa, fase, coordenadaInicial)
                 }
             }, 1);
-
-            fase++
-            start(mapa, fase, coordenadaInicial)
             break;
     
         case 2:
-           
+            player.coordenada = {x:37, y:75}
+            
+            mapa = construirMapa1(mapa, fase)
+            chave[0] = new Chave({x: 21, y:55}, false, {x: 45, y: 66})
+            chave[1] = new Chave({x: 17, y:76}, false, {x: 16, y: 57})
+            botao = new Botao({x: 29, y: 55}, {x: 23, y: 76})
+
+            console.log("sasdas")
+            const intervalId2 = setInterval(function() {
+                player.coordenada = renderizacao(mapa, player.coordenada)
+                if (morte == true) {
+                    contadorMorte++
+                    morte = false
+                    clearInterval(intervalId2)
+                    start(mapa, fase, coordenadaInicial)
+                } else if(contadorMorte == 3){
+                    clearInterval(intervalId2)
+                    window.location.replace("../templated/home.html") // Pagina do game over de derrota colocar depois
+                    alert("Voce perdeu KKKKKKKKKKKKKKKKKKKKKKKKKK")
+                } else if ( (player.coordenada.x == chave[0].porta.x) && (player.coordenada.y == chave[0].porta.y) ) {
+                    clearInterval(intervalId2)
+                    alert("Fase 2 Concluida")
+                }
+            }, 1);
             break;
 
         case 3:
@@ -238,7 +362,12 @@ function Chave(coordenada, playerComAChave, porta)
     this.porta = porta
     this._porta = document.getElementById(`${porta.x}-${porta.y}`)
     this._porta.textContent = 'D'
-    
-   
+}
+
+function Botao(coordenada, porta)
+{
+    this.coordenada = coordenada
+    this.porta = porta
+    this._porta = document.getElementById(`${porta.x}-${porta.y}`)
 }
 
